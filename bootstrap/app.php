677e888fbd2376467a75322148d43c3b22d22355
tuanngoc1601 <tuanngoc12340@gmail.com>
2024-08-12 14:28:16 +0700

    add db

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,7 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\ValidatePostSize::class,
             \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
             \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-            Cors::class,
+            // Cors::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
