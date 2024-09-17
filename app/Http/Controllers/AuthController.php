920323f69c87ddd1180889e9a2d054f53d2d3f3b
tuanngoc1601 <tuanngoc12340@gmail.com>
2024-09-17 17:26:49 +0700

    fix: config refresh token expried time and response

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -61,7 +61,7 @@ class AuthController extends Controller
     {
         return response()->json([
             'message' => "Ok",
-            'user' => Auth::user(),
+            'data' => Auth::user(),
         ], 200);
     }
 
