80fc2b6e5fb1b104280858a2923a2ffda592c420
tuanngoc1601 <tuanngoc12340@gmail.com>
2024-10-22 22:25:11 +0700

    add flash sale product detail api

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,11 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $flash_sale = $this->flash_sales()
+            ->where('time_start', '<=', Carbon::now())
+            ->where('time_end', '>=', Carbon::now())
+            ->first();
+
         return [
             'id' => $this->getHashedIdAttribute(),
             'name' => $this->name,
@@ -24,6 +30,8 @@ class ProductResource extends JsonResource
             'flash_sale_price' => $this->flash_sale_price,
             'stock_quantity' => $this->stock_quantity,
             'sold_quantity' => $this->sold_quantity,
+            'is_flash_sales' => $flash_sale ? true : false,
+            'flash_sale_end_time' => $flash_sale ? $flash_sale->time_end : null,
             'images' => $this->whenLoaded('medias', function () {
                 return $this->medias->map(function ($image) {
                     return [
